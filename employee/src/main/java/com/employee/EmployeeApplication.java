560773daebedf59b8d21b775ccd97a4e9bcdad11
HariPrasad888 <omkaramhari18@gmai.com>
2022-11-30 10:37:21 +0530

    version 1.0

---
+++ employee/src/main/java/com/employee/EmployeeApplication.java
@@ -0,0 +1,18 @@
+package com.employee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	private final static Logger log= LoggerFactory.getLogger(EmployeeApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+		log.error("Error occured while starting the application");
+	}
+
+}
