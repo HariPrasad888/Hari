827a6cf3aa76f167fa208bff9b8c34e21ed14435
HariPrasad888 <omkaramhari18@gmai.com>
2022-12-01 13:36:04 +0530

    version 1.3

--- employee/src/main/java/com/employee/service/EmployeeService.java
+++ employee/src/main/java/com/employee/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.employee.service;
 
-public class EmployeeService {
+import com.employee.pojo.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+    public Employee findById(int id);
+    public void add(List<Employee> employees);
+    public void deleteAll();
+    public void deleteById(int id);
 }
---
+++ employee/src/main/java/com/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.employee.service;
+
+import com.employee.pojo.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(List<Employee> employees) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
